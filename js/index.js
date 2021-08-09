8414693f0946badf27cd1c453f5f5f4f0c669ed4
Nick07242000 <nicohidalgo2000@gmail.com>
2021-08-09 16:12:51 -0300

    First Update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
+document.getElementById("menuToggleBox").addEventListener("change", function() {
+  if (document.getElementById("menuToggleBox").checked == true) {
+    document.getElementsByTagName("main")[0].setAttribute("style", "background: #000; opacity:0.5; z-index:10;");
+  } else {
+    document.getElementsByTagName("main")[0].setAttribute("style", "background: none");
+  }
+});
+
 function initMap() {
-    const uluru = { lat: -25.344, lng: 131.036 };
+    const uluru = { lat: -31.41989510155919, lng: -64.18650345528462 };
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
+      zoom: 13,
       center: uluru,
     });
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
+    const spotsArray = [
+      {
+        lat: -31.438363227089752,
+        lng: -64.18884800203924
+      }
+    ]
+    for (let i = 0; i < spotsArray.length; i++) {
+      const marker = new google.maps.Marker({
+        position: spotsArray[i],
+        map: map,
+      })
+    }
+};
